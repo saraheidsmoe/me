efdfaede6a248907af469ab2e3a87cc4f18aa492
saraheidsmoe <sarahpoepping@gmail.com>
2017-07-16 20:40:21 -0500

    New site

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
-$(document).ready(function(){
-	$('.owl-carousel').owlCarousel({
-		items: 1,
-		margin: 10,
-		loop: true
+$( document ).ready(function() {
+	$("#sideNav a").click(function () {
+		var isActiveTab = $(this).hasClass("active")
+		console.log("isActiveTab " + isActiveTab);
+		if (!isActiveTab) {
+			$("#sideNav ul a").removeClass("active");
+			$(this).addClass("active");
+		}
+
+		var idname = $(this).data('divid');
+		$("#" + idname).show().siblings().hide();
 	});
 });
