219e1d4cdb2512b958f4ed30bb56517535d9c633
L Block <dillon@lblock.design>
2017-08-01 23:08:50 -0500

    Initial responsive code

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 $( document ).ready(function() {
+	// Switch out main content when clicking on side nav
 	$("#sideNav a").click(function () {
-		var isActiveTab = $(this).hasClass("active")
-		console.log("isActiveTab " + isActiveTab);
+		var isActiveTab = $(this).hasClass("active");
+		var idname = $(this).data('divid');
+
 		if (!isActiveTab) {
 			$("#sideNav ul a").removeClass("active");
 			$(this).addClass("active");
 		}
 
-		var idname = $(this).data('divid');
+		// Scroll to photos
+		$('html, body').animate({
+			scrollTop: $("#mainContent").offset().top
+		}, 2000);
+
+		// Show related photos, hide others
 		$("#" + idname).show().siblings().hide();
 	});
 });
